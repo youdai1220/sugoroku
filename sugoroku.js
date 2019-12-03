69249f89f2cd50a27a8ee7b77c3d0f8e986a1419
youdai1220 <46952406+youdai1220@users.noreply.github.com>
2019-12-03 23:54:13 +0900

    Update sugoroku.js

--- sugoroku.js
+++ sugoroku.js
@@ -1,9 +1,8 @@
 (function(){
     'use strict';
-    const b1 = document.getElementById('b1');
     const saikoro = document.getElementById('saikoro'); 
     document.getElementById('a1').style.backgroundColor = 'skyblue';
-    document.getElementById('a50').style.backgroundcolor = 'orange';
+    document.getElementById('a50').style.backgroundColor = 'orange';
 
     var a = 1;
 
@@ -18,17 +17,32 @@
     a1.appendChild(paragraph)
 
     saikoro.onclick = () => {
-        if(a!==1){
+        if(a === 1){
+            removeAllChildren(a1);
+            const paragraph = document.createElement('p');
+            paragraph.innerText = 'Start';
+            a1.appendChild(paragraph);
+        }else if(a > 1 && a < 50){
+            removeAllChildren(eval(String('a'+ a)));
             document.getElementById(String('a'+ a)).style.backgroundColor = 'white';
+        }else if(a === 50){
+            removeAllChildren(a50);
+            const paragraph = document.createElement('p');
+            paragraph.innerText = 'Goal';
+            a50.appendChild(paragraph);
         }
 
         a = a + Math.floor(Math.random()*6)+1
-        console.log(a);
-
-        const paragraph = document.createElement('p');
-        paragraph.innerText = 'A'
-        eval(String('a'+a)).appendChild(paragraph)
-
-        document.getElementById(String('a'+ a)).style.backgroundColor = 'orange';
+        if(a > 50){
+            a = 50;
+            const paragraph = document.createElement('p');
+            paragraph.innerText = 'A'
+            eval(String('a'+ a)).appendChild(paragraph)
+        }else{
+            const paragraph = document.createElement('p');
+            paragraph.innerText = 'A'
+            eval(String('a'+ a)).appendChild(paragraph)
+            document.getElementById(String('a'+ a)).style.backgroundColor = 'yellowgreen';
+        };
     }
 })();

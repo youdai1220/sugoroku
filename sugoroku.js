9d105fd58c71fd3233ec8edc43e927149f992e45
youdai1220 <46952406+youdai1220@users.noreply.github.com>
2019-12-25 16:33:01 +0900

    Update sugoroku.js

--- sugoroku.js
+++ sugoroku.js
@@ -2,6 +2,7 @@
     'use strict';
     const saikoro = document.getElementById('saikoro'); 
     const turnArea = document.getElementById('turnarea');
+    const resultArea = document.getElementById('result-area');
     document.getElementById('a1').style.backgroundColor = 'skyblue';
     document.getElementById('a50').style.backgroundColor = 'orange';
 
@@ -14,6 +15,7 @@
     const paragraphA = document.createElement('p'); //Aがいるマスの内容
     const paragraphB = document.createElement('p'); //Bがいるマスの内容   
     const paragraph50 = document.createElement('p'); //Goalの内容
+    const result = document.createElement('h4'); //resultの内容
 
     function removeAllChildren(element){
         while (element.firstChild){
@@ -28,9 +30,15 @@
     Turn.innerText = 'Aさんのターンです。'
     turnArea.appendChild(Turn); //最初のターンはAから
 
-    saikoro.onclick = () => { //ボタンを押したときの処理
+    saikoro.onclick = () =>{ //ボタンを押したときの処理
+
+        function TurnChangeToB(){ //Bのターンに変える処理
+            removeAllChildren(turnArea);
+            Turn.innerText = 'Bさんのターンです。'
+            turnArea.appendChild(Turn);
+        };
+
         if(turn % 2 === 1){ //trueならAさんのターン
-            turn = turn + 1; //ターンが順番になるようにするための処理
             if(a === 1){ //Aがスタートするときの処理
                 paragraph1.innerText = 'Start \n \n B';
                 a1.appendChild(paragraph1); //Startマスに残る文字列は「Start  B」
@@ -46,37 +54,43 @@
                     document.getElementById(String('a'+ a)).style.backgroundColor = 'white'; //Aがいなくなったマスは元の状態になる
                 }
 
-            }else if(a === 50){ //すでにゴールしている場合はBのターンに変わる
-                removeAllChildren(turnArea);
-                Turn.innerText = 'Bさんのターンです。'
-                turnArea.appendChild(Turn);
-                return;
             }
 
             a = a + Math.floor(Math.random()*6)+1 //乱数で1~6までが出る
-            if(a >= 50 && b < 50){ //Aがゴールし、Bがまだゴールしていないときの処理
+            if(a >= 50){
                 a = 50;
                 removeAllChildren(a50);
                 paragraph50.innerText = 'Goal \n \n A' 
                 a50.appendChild(paragraph50) //Goalの内容は「Goal　A」
 
-            }else if(a >= 50 && b === 50){ //Aがゴールし、Bがすでにゴールしているときの処理
-                a = 50;
-                removeAllChildren(a50);
-                paragraph50.innerText = 'Goal \n \n A B'
-                a50.appendChild(paragraph50) //Goalの内容は「Goal　A　B」
-            }else{ //AもBもゴールしていないときの処理
+                result.innerText = 'Aさんが' + (a - b) + 'マス差で勝ちました。おめでとうございます。'
+                resultArea.appendChild(result);
+
+                document.getElementById('saikoro').disabled = true;
+            }
+
+            else{ //AもBもゴールしていないときの処理
                 paragraphA.innerText = 'A'
                 eval('a'+ a).appendChild(paragraphA) //Aのいるマスの内容は「A」
                 document.getElementById(String('a'+ a)).style.backgroundColor = 'yellowgreen'; //Aのいるマスは黄緑になる
             };
 
-            removeAllChildren(turnArea); //Aのターン終了後に表示が変わる
-            Turn.innerText = 'Bさんのターンです。'
-            turnArea.appendChild(Turn);
+            if(b !== 50){
+                turn = turn + 1; //ターンが順番になるようにするための処理
+                TurnChangeToB();
+                return;
+            }else if(b === 50){
+                return;
+            }
 
         }else if(turn % 2 === 0){ //trueならBのターン
-            turn = turn + 1;
+
+            function TurnChangeToA(){ //Bのターンに変える処理
+                removeAllChildren(turnArea);
+                Turn.innerText = 'Aさんのターンです。'
+                turnArea.appendChild(Turn);
+            };
+
             if(b === 1){
                 paragraph1.innerText = 'Start';
                 a1.appendChild(paragraph1);
@@ -88,38 +102,36 @@
                     eval('a'+ b).appendChild(paragraphSame); 
 
                 }else{
-                    removeAllChildren(eval(String('a'+ b)));
+                    removeAllChildren(eval('a'+ b));
                     document.getElementById(String('a'+ b)).style.backgroundColor = 'white';
                 }
-
-            }else if(b === 50){
-                removeAllChildren(turnArea);
-                Turn.innerText = 'Aさんのターンです。'
-                turnArea.appendChild(Turn);
-                return;
             }
 
             b = b + Math.floor(Math.random()*6)+1
-            if(b >= 50 && a < 50){
-                b = 50;
-                paragraph50.innerText = 'B'
-                a50.appendChild(paragraph50)
-
-            }else if(b >= 50 && a === 50){
+            if(b >= 50){
                 b = 50;
                 removeAllChildren(a50);
-                paragraph50.innerText = 'Goal \n \n A B'       
-                a50.appendChild(paragraph50)         
+                paragraph50.innerText = 'Goal \n \n B' 
+                a50.appendChild(paragraph50) //Goalの内容は「Goal　B」
 
+                result.innerText = 'Bさんが' + (b - a) + 'マス差で勝ちました。おめでとうございます。'
+                resultArea.appendChild(result);
+
+                document.getElementById('saikoro').disabled = true;
             }else{
                 paragraphB.innerText = 'B'
                 eval(String('a'+ b)).appendChild(paragraphB)
                 document.getElementById(String('a'+ b)).style.backgroundColor = 'pink';
             };
 
-            removeAllChildren(turnArea);
-            Turn.innerText = 'Aさんのターンです。'
-            turnArea.appendChild(Turn);
+            if(a !== 50){
+                turn = turn + 1; //ターンが順番になるようにするための処理
+                TurnChangeToA();
+                return;
+            }else if(a === 50){
+                return;
+            }
+
         }
     }
 })();

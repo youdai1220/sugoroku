436d2ed069b88df89f1d8adf7798fdcb03b6a049
youdai1220 <46952406+youdai1220@users.noreply.github.com>
2019-12-28 00:12:21 +0900

    Update sugoroku.js

--- sugoroku.js
+++ sugoroku.js
@@ -7,15 +7,15 @@
     document.getElementById('a1').style.backgroundColor = 'skyblue';
     document.getElementById('a50').style.backgroundColor = 'orange';
 
-
-    var a;
-    var b;
+    var eventNumber; //イベントを発生させるマス
+    var shortcut;
     var aTotal = 1; //Aの位置
     var bTotal = 1; //Bの位置
     var turn; //A・Bのターン
 
-    var number = document.createElement('h2'); //NumberAreaの内容
+    var number = document.createElement('h1'); //NumberAreaの内容
     var Turn = document.createElement('h4'); //turnAreaの内容
+    var eventParagraph = document.createElement('div'); //eventの内容(x1 ~ x50)
     const paragraph1 = document.createElement('p'); //Startの内容
     const paragraphSame = document.createElement('p'); //AとBが同じマスにいるときの内容
     const paragraphA = document.createElement('p'); //Aがいるマスの内容
@@ -34,29 +34,41 @@
     Turn.innerText = '「ゲームを始める」を押してください。'
     turnArea.appendChild(Turn); 
 
-    startButton.onclick = () => { //先攻・後攻を決める
-        var x = Math.random(); 
+    startButton.onclick = () => { //先攻・後攻を決め、イベントを発生させる
+        var x = Math.random(); //xとyという2つの乱数を発生させる
         var y = Math.random();
-        if(x > y){
+        if(x > y){ //x > yの場合、Aが先攻
             turn = 1;
             Turn.innerText = 'Aさんのターンです。'
-        }else if(y > x){
+        }else if(y > x){ //y > xの場合、Bが先攻
             turn = 2;
             Turn.innerText = 'Bさんのターンです。'
         }
         turnArea.appendChild(Turn); 
+
+        eventNumber = Math.floor(Math.random() * 46) + 2; //乱数を発生させ、ランダムにイベントを発生させる
+        shortcut = Math.floor(Math.random() * 4) + 2; 
+        eventParagraph.innerText = shortcut + 'マス進む'
+        eval('x'+ eventNumber).appendChild(eventParagraph);
+
         document.getElementById('saikoro').disabled = false; //サイコロを振れるようにする
         document.getElementById('start-button').disabled = true; //リスタートをできないようにする
     }
 
 
-    saikoro.onclick = () =>{ //ボタンを押したときの処理
+    saikoro.onclick = () =>{ //「サイコロを振る」が押されてから処理を少し遅らせる
+        document.getElementById('saikoro').disabled = true; //サイコロを振れないようにする
+        setTimeout(dice, 300);
+    };
+
+    function dice(){ //「サイコロを振る」を押したときの処理
         if(turn % 2 === 1){ //trueならAさんのターン
 
             function TurnChangeToB(){ //Bのターンに変える処理
                 removeAllChildren(turnArea);
                 Turn.innerText = 'Bさんのターンです。'
                 turnArea.appendChild(Turn);
+                document.getElementById('saikoro').disabled = false; //サイコロを振れるようにする
             };
 
             if(aTotal === 1){ //Aがスタートするときの処理
@@ -83,11 +95,11 @@
 
             }
 
-            a = Math.floor(Math.random() * 6) + 1 //乱数で1~6までが出る
             removeAllChildren(NumberArea);
+            var a = Math.floor(Math.random() * 6) + 1 //乱数で1~6までが出る
             number.innerText = a;
-            NumberArea.appendChild(number); //出た目はNUmberAreaに表示する
             aTotal = aTotal + a;
+            NumberArea.appendChild(number); //出た目はNumberAreaに表示する
 
             if(aTotal >= 50){ //Aがゴールするときの処理
                 aTotal = 50;
@@ -95,25 +107,48 @@
                 paragraph50.innerText = 'Goal \n \n A' 
                 a50.appendChild(paragraph50) //Goalの内容は「Goal　A」
 
+                removeAllChildren(turnArea);
+                Turn.innerText = 'Aさんの勝利です。'
+                turnArea.appendChild(Turn);
+
                 function Result(){
-                    const result = 'Aさんが' + (aTotal - bTotal) + 'マス差で勝ちました。おめでとうございます。' //Aが先にゴールした時の表示
-                    alert(result);
+                    const result = 'Aさんが' + (aTotal - bTotal) + 'マス差で勝ちました。おめでとうございます。\nリスタートする場合はOKをクリックしてください。' //Aが先にゴールした時の表示
+                    var restart = confirm(result); //リスタートしてもよいか確認
+                    if(restart){ //OKが入力されればページをリロード
+                        location.reload();
+                    }
                 };
 
-                setTimeout(Result,1)
+                setTimeout(Result,10)
                 document.getElementById('saikoro').disabled = true; //Aがゴールしたらサイコロは振れなくなる
+                return;
             }
 
             else{ //Aがゴールしないときの処理
                 paragraphA.innerText = 'A'
                 eval('a'+ aTotal).appendChild(paragraphA); //Aのいるマスの内容は「A」
                 document.getElementById(String('a'+ aTotal)).style.backgroundColor = 'yellowgreen'; //Aのいるマスは黄緑になる
+
+                if(aTotal === eventNumber){
+                    var event = () => {
+                        removeAllChildren(NumberArea);
+                        number.innerText = shortcut + 'マス進む';
+                        NumberArea.appendChild(number);
+
+                        removeAllChildren(eval('a'+ aTotal));
+                        document.getElementById(String('a'+ aTotal)).style.backgroundColor = 'white';
+                        aTotal = aTotal + shortcut;
+
+                        paragraphA.innerText = 'A';
+                        eval('a'+ aTotal).appendChild(paragraphA);
+                        document.getElementById(String('a'+ aTotal)).style.backgroundColor = 'yellowgreen';
+                    }
+                    setTimeout(event, 500);
+                }
+                turn = turn + 1; //ターンが順番になるようにするための処理
+                TurnChangeToB(); //Aのターン終了後Bのターンに変わる
+                return;
             };
-
-            turn = turn + 1; //ターンが順番になるようにするための処理
-            TurnChangeToB(); //Aのターン終了後Bのターンに変わる
-            return;
-
 
         //以下、Bのターンについてーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーー
 
@@ -123,6 +158,7 @@
                 removeAllChildren(turnArea);
                 Turn.innerText = 'Aさんのターンです。'
                 turnArea.appendChild(Turn);
+                document.getElementById('saikoro').disabled = false; //サイコロを振れるようにする
             };
 
             if(bTotal === 1){ //Bがスタートするときの処理
@@ -144,40 +180,65 @@
 
                 }else{
                     removeAllChildren(eval('a'+ bTotal));
-                    document.getElementById(String('a'+ bTotal)).style.backgroundColor = 'white'; //Aがいなくなったマスは元の状態になる
+                    document.getElementById(String('a'+ bTotal)).style.backgroundColor = 'white'; //Bがいなくなったマスは元の状態になる
                 }
             }
 
-            b = Math.floor(Math.random() * 6) + 1; //乱数で1~6までが出る
             removeAllChildren(NumberArea);
+            var b = Math.floor(Math.random() * 6) + 1; //乱数で1~6までが出る
             number.innerText = b;
-            NumberArea.appendChild(number); //出た目はNUmberAreaに表示する
             bTotal = bTotal + b;
+            NumberArea.appendChild(number); //出た目はNUmberAreaに表示する
 
-            if(bTotal >= 50){
+            if(bTotal >= 50){ //Bがゴールするときの処理
                 bTotal = 50;
                 removeAllChildren(a50);
                 paragraph50.innerText = 'Goal \n \n B' 
                 a50.appendChild(paragraph50) //Goalの内容は「Goal　B」
 
+                removeAllChildren(turnArea);
+                Turn.innerText = 'Bさんの勝利です。'
+                turnArea.appendChild(Turn);
+
                 function Result(){
-                    const result = 'Bさんが' + (bTotal - aTotal) + 'マス差で勝ちました。おめでとうございます。' //Bが先にゴールした時の表示
-                    alert(result);
+                    const result = 'Bさんが' + (bTotal - aTotal) + 'マス差で勝ちました。おめでとうございます。\nリスタートする場合はOKをクリックしてください。' //Bが先にゴールした時の表示
+                    var restart = confirm(result); //リスタートしてもよいか確認
+                    if (restart){ //OKが入力されればページをリロード
+                        location.reload();
+                    };
                 };
 
-                setTimeout(Result,1);
+                setTimeout(Result,10);
                 document.getElementById('saikoro').disabled = true; //Aがゴールしたらサイコロは振れなくなる
+                return;
 
             }else{ //Bがゴールしないときの処理
                 paragraphB.innerText = 'B';
                 eval(String('a'+ bTotal)).appendChild(paragraphB); //Bのいるマスの内容は「B」
                 document.getElementById(String('a'+ bTotal)).style.backgroundColor = 'pink'; //Bのいるマスはピンクになる
+
+                if(bTotal === eventNumber){
+                    var event = () => {
+                        removeAllChildren(NumberArea);
+                        number.innerText = shortcut + 'マス進む';
+                        NumberArea.appendChild(number);
+
+                        removeAllChildren(eval('a'+ bTotal));
+                        document.getElementById(String('a'+ bTotal)).style.backgroundColor = 'white';
+                        bTotal = bTotal + shortcut;
+
+                        paragraphB.innerText = 'B';
+                        eval('a'+ bTotal).appendChild(paragraphB);
+                        document.getElementById(String('a'+ bTotal)).style.backgroundColor = 'pink';
+                    }
+                    setTimeout(event, 500);
+                }
+            
+                turn = turn + 1; //ターンが順番になるようにするための処理
+                TurnChangeToA(); //Bのターン終了後Aのターンに変わる
+                return;
             };
 
-            turn = turn + 1; //ターンが順番になるようにするための処理
-            TurnChangeToA(); //Bのターン終了後Aのターンに変わる
-            return;
-            
         }
     }
 })();
